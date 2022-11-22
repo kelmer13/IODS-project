44b144e2d4fae9e558ca6bb653938eec554ad5fd
K E <ke@Ks-MacBook-Air.local>
2022-11-22 14:15:56 +0800

    assignment 3 submit

--- data/create_alc.R
+++ data/create_alc.R
@@ -5,9 +5,6 @@
 #   Link: https://archive.ics.uci.edu/ml/datasets/Student+Performance
 
 # Data Wrangling, step 3
-
-
-
 ```{r}
 # "Read both student-mat.csv and student-por.csv into R (from the data folder)"
 studentmat <- read.table("https://raw.githubusercontent.com/KimmoVehkalahti/Helsinki-Open-Data-Science/master/datasets/student-mat.csv", sep=";", header=TRUE)
@@ -21,26 +18,49 @@ str(studentpor)
 dim(studentpor)
 ```
 
+# Data Wrangling, step 5
 ```{r}
-# Join the two data sets using all other variables than "failures", "paid", "absences", "G1", "G2", "G3" as (student) identifiers. 
-# Keep only the students present in both data sets.
+#Get rid of the duplicate records in the joined data set. 
 
-# access the dplyr package
-library(dplyr)
+newalc <- select(joined, all_of(join_cols))
+for(col_name in free_cols) {
+  two_cols <- select(joined, starts_with(col_name))
+  first_col <- select(two_cols, 1)[[1]]
+  if(is.numeric(first_col)) {
+    newalc[col_name] <- round(rowMeans(two_cols))
+  } else {
+    newalc[col_name] <- first_col
+  }
+}
 
-free_cols <- c("failures", "paid", "absences", "G1", "G2", "G3")
+```
 
-join_cols <- setdiff(colnames(studentpor), free_cols)
+# Data Wrangling, step 6
 
-# join the two data sets by the selected identifiers
-math_por <- inner_join(math, por, by = join_cols)
+```{r}
+# Take the average of the answers related to weekday and weekend alcohol consumption to create a new column 'alc_use' to the joined data. 
 
-# look at the column names of the joined data set
-colnames(math_por)
+newalc <- mutate(newalc, alc_use = (Dalc + Walc) / 2)
 
-# glimpse at the joined data set
-glimpse(math_por)
+# Then use 'alc_use' to create a new logical column 'high_use' which is TRUE for students for which 'alc_use' is greater than 2 (and FALSE otherwise)
 
+newalc <- mutate(newalc, high_use = alc_use > 2)
+
+```
+
+# Data Wrangling, step 7
+
+
+```{r}
+# Glimpse at the joined and modified data to make sure everything is in order. 
+
+glimpse(newalc) 
+
+# Results: newalc has 370 rows and 37 columns.
+
+# Save the joined and modified data set to the ‘data’ folder, using for example write_csv() function (readr package, part of tidyverse).
+
+write_csv(x = newalc, "data/newalc.csv")
 
 ```
 
